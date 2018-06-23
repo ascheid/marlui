cc3fdba84f7dbc6be73eda7f47208f57f73f4704
alvaro.scheid <ascheid@gmail.com>
2018-06-23 12:54:06 -0300

    Project Start

---
+++ src/main/java/com/alvaroscheid/marlui/MarluiApplication.java
@@ -0,0 +1,12 @@
+package com.alvaroscheid.marlui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarluiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarluiApplication.class, args);
+	}
+}
